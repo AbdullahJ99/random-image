80f8029fbcac1e7c0155c7e4a707a67939508244
Abdullah_J <abdullahj1899@gmail.com>
2023-07-29 19:16:04 -0400

    Images appear after clicking button

--- js/script.js
+++ js/script.js
@@ -5,6 +5,21 @@ const img = document.querySelector(".img");
 const getImg = async function(){
     const res = await fetch("https://picsum.photos/v2/list?limit=100"); 
     const images = await res.json();
+    selectRandomImage(images);
     console.log(images);
 };
-getImg();
+const selectRandomImage = function(images){
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    displayImage(randomImage);
+};
+const displayImage = function(randomImage){
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+}
+button.addEventListener("click", function(){
+    getImg();
+});
